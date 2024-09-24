8954d4925aeeda40ad484bdd9ac06a2d1359110e
chailana <chailana35217793@gmail.com>
2024-09-24 20:57:18 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -45,8 +45,8 @@ async def youtube_dl_call_back(bot, update):
      await bot.send_message(
         chat_id=update.message.chat.id,
         text="Please reply to a message containing a valid URL.",
-        parse_mode=ParseMode.HTML,
-        reply_to_message_id=update.id if update.message is None else update.message.message_id,
+        parse_mode=ParseMode.MARKDOWN,
+        reply_to_message_id=update.id if update.message is None else update.message.id,
     )
     return
     
