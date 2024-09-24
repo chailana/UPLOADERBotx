66eff9d8599ec242754fca5948714b5014beebf8
chailana <chailana35217793@gmail.com>
2024-09-24 17:44:33 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -249,7 +249,6 @@ async def youtube_dl_call_back(bot, update):
             message_id=update.message.message_id,
             disable_web_page_preview=True)
 
-#=================================
 
 async def clendir(directory):
 
@@ -261,5 +260,3 @@ async def clendir(directory):
         os.remove(directory)
     except:
         pass
-
-#=================================
